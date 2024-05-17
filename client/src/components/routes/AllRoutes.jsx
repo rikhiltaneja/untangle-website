f97d7c81801f6ebc170c7f91f713722c3d36fcea
aayushwrld <aayush.arora@kalvium.community>
2024-05-17 09:35:28 +0530

    Frontend Setup Done

---
+++ client/src/components/routes/AllRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from '../Homepage'
+
+function AllRoutes() {
+  return (
+    <>
+    <Routes>
+        <Route path='/' element={<Homepage />}/>
+    </Routes>
+    </>
+  )
+}
+
+export default AllRoutes
